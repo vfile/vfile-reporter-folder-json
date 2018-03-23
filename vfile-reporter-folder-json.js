34c10cb990c377f57752c7d0b5221be22ac31ffe
Christian Murphy <cmurphy@unicon.net>
2018-03-23 08:34:29 -0700

    initial commit

---
+++ vfile-reporter-folder-json.js
@@ -0,0 +1,49 @@
+const { sep } = require("path");
+
+class vFolder {
+  constructor({ name = "", children = [] } = {}) {
+    return {
+      type: "folder",
+      name,
+      children
+    };
+  }
+}
+
+// NOTE: this relies on side effects from the children property
+function addFileOrFolder(folder, { pathList, vFile }) {
+  // this is where the file goes, add it
+  if (pathList.length <= 1) {
+    vFile.type = "file";
+    folder.children.push(vFile);
+    return folder;
+  }
+
+  // find sub folder
+  const name = pathList.shift();
+  let subFolder = folder.children.find(child => child.name === name);
+
+  // folder is missing add it
+  if (!subFolder) {
+    subFolder = new vFolder({ name });
+    folder.children.push(subFolder);
+  }
+
+  // recurse
+  addFileOrFolder(subFolder, { pathList, vFile });
+
+  // return resolved virtual file system
+  return folder;
+}
+
+/**
+ * Returns a Unist compatible JSON syntax tree.
+ * With `file` and `folder` nodes
+ */
+function vFilesystem(files) {
+  return files
+    .map(vFile => ({ vFile, pathList: vFile.path.split(sep) }))
+    .reduce(addFileOrFolder, new vFolder());
+}
+
+module.exports = vFilesystem;
